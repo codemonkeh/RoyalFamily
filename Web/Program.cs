52dff2b3c0f5141c479769ae0b9af0e6ac0c23c9
codemonkeh <14917019+codemonkeh@users.noreply.github.com>
2018-06-20 14:48:55 +1000

    Added test data and made data filename configurable

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,13 +1,17 @@
-﻿using RoyalFamily.Common.Data.Repositories;
+﻿using System.IO;
+using RoyalFamily.Common.Data.Repositories;
 using RoyalFamily.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RoyalFamily.Web
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; set; }
+
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
@@ -15,12 +19,18 @@ namespace RoyalFamily.Web
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                
+
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+                Configuration = builder.Build();
+
                 // As we are using an in-memory database for this exercise we must seed it on start-up.
                 // In a real-world application this would be unnecessary
+                var dataFilename = Configuration["Data:TestDataFilename"];
                 var personRepository = services.GetService<IPersonRepository>();
                 var importer = new JsonDataImporter(personRepository);
-                importer.LoadPeopleFromJsonFile(@"testdata.json").Wait();
+                importer.LoadPeopleFromJsonFile(dataFilename).Wait();
             }
 
             host.Run();
